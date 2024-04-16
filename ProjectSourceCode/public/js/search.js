d798dd265dbe48ee87b25cfed3e3dbabe8c319ac
Ayla Tabi <ayta5739@colorado.edu>
2024-04-16 09:42:17 -0600

    got some card formating working

--- ProjectSourceCode/public/js/search.js
+++ ProjectSourceCode/public/js/search.js
@@ -85,12 +85,15 @@ async function make_pokemon_card(name) {
     const special_attack = stats[3].base_stat;
     const special_defense = stats[4].base_stat;
     const speed = stats[5].base_stat;
-    const types_string = data.types.map(obj => obj.type.name).join(', ');
+    const types_string = data.types.map(obj => obj.type.name);
+    const types_array = types_string.join(', ');
+    const get_one_type = types_array[0]
 
+    console.log(get_one_type)
  return elt('div', {class: "container"}, "", 
             elt('div', {class: "row"}, "", 
                 elt('div', {class: "col-md-4", style: "margin-top:20px; margin-bottom:20px;"}, "",
-                    elt('div', {class: `card pokemon-card ${type}`, width: "18rem", style: "" }, "",
+                    elt('div', {class: `card pokemon-card ${get_one_type}`, width: "18rem", style: "" }, "",
                             elt('img', {className: "card-img-top", src: img_url, alt:"Card image cap" }, ""),
                             elt ('div', {class : "card-body"}, '', 
                                 elt('h5', {class: 'card-title', id:'pokemon_name'}, capitalize(name)),
