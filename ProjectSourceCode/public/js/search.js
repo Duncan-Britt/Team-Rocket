76c1f561a0ca3ab642a7982100cb12ffc5bc4624
Duncan Britt <dbru997@gmail.com>
2024-04-16 23:01:45 -0600

    Merging

--- ProjectSourceCode/public/js/search.js
+++ ProjectSourceCode/public/js/search.js
@@ -89,23 +89,23 @@ async function make_pokemon_card_local(name) {
     const speed = stats[5].base_stat;
     const types_string = data.types.map(obj => obj.type.name).join(', ');
 
- return elt('div', {class: "container"}, "", 
-            elt('div', {class: "row"}, "", 
-                elt('div', {class: "col-md-4", style: "margin-top:20px; margin-bottom:20px;"}, "",
-                    elt('div', {class: "card pokemon-card", width: "18rem", style: "background-image: url('/images/orange_bg.jpg')" }, "",
-                            elt('img', {className: "card-img-top", src: img_url, alt:"Card image cap" }, ""),
-                            elt ('div', {class : "card-body"}, '', 
+    return elt('div', {class: "container"}, "", 
+               elt('div', {class: "row"}, "", 
+                   elt('div', {class: "col-md-4", style: "margin-top:20px; margin-bottom:20px;"}, "",
+                       elt('div', {class: "card pokemon-card", width: "18rem", style: "background-image: url('/images/orange_bg.jpg')" }, "",
+                           elt('img', {className: "card-img-top", src: img_url, alt:"Card image cap" }, ""),
+                           elt ('div', {class : "card-body"}, '', 
                                 elt('h5', {class: 'card-title', id:'pokemon_name'}, capitalize(name)),
                                 elt('p', {class: 'card-text type'}, `Type(s): ${types_string}`)
-                            ),
-                            elt ('ul', {class : "list-group list-group-flush", id: 'pokemon_description'}, '', 
+                               ),
+                           elt ('ul', {class : "list-group list-group-flush", id: 'pokemon_description'}, '', 
                                 elt('li', {class: 'list-group-item stats'}, `Hitpoints: ${hp}    Attack: ${attack}    Speed:${speed}   `),
                                 elt('li', {class: 'list-group-item stats'}, `Special Attack: ${special_attack} \n Special Defense: ${special_defense}`)
-                            )
-                        )
-                    )
-                )
-            );
+                               )
+                          )
+                      )
+                  )
+              );
 
 }
 
