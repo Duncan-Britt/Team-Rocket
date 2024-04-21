bbaedfec18ffc0da8062acd6c5913134729570d8
kiwickersham <94425658+kiwickersham@users.noreply.github.com>
2024-04-20 23:19:03 -0600

    filter by type implementation

--- ProjectSourceCode/public/js/search.js
+++ ProjectSourceCode/public/js/search.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const search_input_field = document.getElementById("search_input_field");    
     const pokemon_container = document.getElementById("pokemon-container");
     const login_data_element = document.getElementById("is-logged-in");
+    const form = document.forms[0];
+    const radios = form.elements["type-choice"];
     const is_logged_in = login_data_element.dataset.isloggedin == "true" ? true : false;
         
     
@@ -19,58 +21,198 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     
     search_button.addEventListener("click", async () => {
+        const type = radios.value;
+        
         const name = search_input_field.value.toLowerCase();
+        while (pokemon_container.firstChild) {
+            pokemon_container.removeChild(pokemon_container.firstChild);
+        }
+        let pkmn = [];
 
-        const pokemon_card = await make_pokemon_card(name);
-        if (!pokemon_card) {
+        if(type != null && type != "none") {
+
+            const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
+            if (!response.ok) {
+                return null;
+            }
+            const data = await response.json();
+
+            let i = 0;
+            while(i < data.pokemon.length) {
+                if(name === "") {
+                    pkmn.push(data.pokemon[i].pokemon.name)
+                }
+                else if (name === data.pokemon[i].pokemon.name) {
+                    pkmn.push(name);
+                }
+                i ++;
+            }
+        }
+        else {
+            pkmn.push(name);
+        }
+
+        if (pkmn.length === 0) {
             pokemon_container.appendChild(
                 elt('p', { style: "color: red" }, "Pokemon not found.")
             );
             return;
         }
+
+        i = 0;
+        while(i < pkmn.length) {
+            let pokemon_card = await make_pokemon_card(pkmn[i]);
+            if (!pokemon_card) {
+                pokemon_container.appendChild(
+                    elt('p', { style: "color: red" }, "Pokemon not found.")
+                );
+                return;
+            }
+
+            pokemon_container.appendChild(pokemon_card);
+            if (is_logged_in) {
+                pokemon_container.appendChild(
+                    elt('form', {
+                        action: "/add",
+                        method: "post",
+                        class: "reg-form",                                        
+                    }, "",
+                        elt('input', {
+                            type: 'hidden',
+                            name: "pokemon",
+                            value: name,
+                        }, ""),
+                        elt('button', {
+                            type: "submit",
+                            class: "btn btn-primary",
+                        }, "Add to collection")
+                    )
+                );
+            } else {
+                pokemon_container.appendChild(
+                    elt('form', {
+                        action: "/add",
+                        method: "post",
+                        class: "reg-form",
+                        hidden: null,
+                    }, "",
+                        elt('input', {
+                            type: 'hidden',
+                            name: "pokemon",
+                            value: name,
+                        }, ""),
+                        elt('button', {
+                            type: "submit",
+                            class: "btn btn-primary",
+                        }, "Add to collection")
+                    )
+                );
+            }
+            i ++;
+        }
+
+        // for(mon in pkmn) {
+        //     pokemon_container.appendChild(
+        //                 elt('p', { style: "color: black" }, mon)
+        //             );
+        //     let pokemon_card = await make_pokemon_card(name);
+        //     if (!pokemon_card) {
+        //         pokemon_container.appendChild(
+        //             elt('p', { style: "color: red" }, "Pokemon not found.")
+        //         );
+        //         return;
+        //     }
+
+        //     pokemon_container.appendChild(pokemon_card);
+        //     if (is_logged_in) {
+        //         pokemon_container.appendChild(
+        //             elt('form', {
+        //                 action: "/add",
+        //                 method: "post",
+        //                 class: "reg-form",                                        
+        //             }, "",
+        //                 elt('input', {
+        //                     type: 'hidden',
+        //                     name: "pokemon",
+        //                     value: name,
+        //                 }, ""),
+        //                 elt('button', {
+        //                     type: "submit",
+        //                     class: "btn btn-primary",
+        //                 }, "Add to collection")
+        //             )
+        //         );
+        //     } else {
+        //         pokemon_container.appendChild(
+        //             elt('form', {
+        //                 action: "/add",
+        //                 method: "post",
+        //                 class: "reg-form",
+        //                 hidden: null,
+        //             }, "",
+        //                 elt('input', {
+        //                     type: 'hidden',
+        //                     name: "pokemon",
+        //                     value: name,
+        //             }, ""),
+        //                 elt('button', {
+        //                     type: "submit",
+        //                     class: "btn btn-primary",
+        //                 }, "Add to collection")
+        //             )
+        //         );
+        //     }
+        // }
         
-        while (pokemon_container.firstChild) {
-            pokemon_container.removeChild(pokemon_container.firstChild);
-        }
-        pokemon_container.appendChild(pokemon_card);
-        if (is_logged_in) {
-            pokemon_container.appendChild(
-                elt('form', {
-                    action: "/add",
-                    method: "post",
-                    class: "reg-form",                                        
-                }, "",
-                    elt('input', {
-                        type: 'hidden',
-                        name: "pokemon",
-                        value: name,
-                    }, ""),
-                    elt('button', {
-                        type: "submit",
-                        class: "btn btn-primary",
-                    }, "Add to collection")
-                   )
-            );
-        } else {
-            pokemon_container.appendChild(
-                elt('form', {
-                    action: "/add",
-                    method: "post",
-                    class: "reg-form",
-                    hidden: null,
-                }, "",
-                    elt('input', {
-                        type: 'hidden',
-                        name: "pokemon",
-                        value: name,
-                    }, ""),
-                    elt('button', {
-                        type: "submit",
-                        class: "btn btn-primary",
-                    }, "Add to collection")
-                   )
-            );
-        }
+        // if (!pokemon_card) {
+        //     pokemon_container.appendChild(
+        //         elt('p', { style: "color: red" }, "Pokemon not found.")
+        //     );
+        //     return;
+        // }
+        
+        // while (pokemon_container.firstChild) {
+        //     pokemon_container.removeChild(pokemon_container.firstChild);
+        // }
+        // pokemon_container.appendChild(pokemon_card);
+        // if (is_logged_in) {
+        //     pokemon_container.appendChild(
+        //         elt('form', {
+        //             action: "/add",
+        //             method: "post",
+        //             class: "reg-form",                                        
+        //         }, "",
+        //             elt('input', {
+        //                 type: 'hidden',
+        //                 name: "pokemon",
+        //                 value: name,
+        //             }, ""),
+        //             elt('button', {
+        //                 type: "submit",
+        //                 class: "btn btn-primary",
+        //             }, "Add to collection")
+        //            )
+        //     );
+        // } else {
+        //     pokemon_container.appendChild(
+        //         elt('form', {
+        //             action: "/add",
+        //             method: "post",
+        //             class: "reg-form",
+        //             hidden: null,
+        //         }, "",
+        //             elt('input', {
+        //                 type: 'hidden',
+        //                 name: "pokemon",
+        //                 value: name,
+        //             }, ""),
+        //             elt('button', {
+        //                 type: "submit",
+        //                 class: "btn btn-primary",
+        //             }, "Add to collection")
+        //            )
+        //     );
+        // }
     });
 });
 
@@ -89,12 +231,7 @@ async function find_pokemon_with_type(name, type) {
         //if no name is specified, retrieve data for every pokemon of specified type
         let res_list = [];
         for(let mon in pkmn_list) {
-            const mon_res = await fetch(mon.pokemon.url);
-            if(!mon_res.ok) {
-                return null;
-            }
-            const mon_data = await mon_res.json();
-            res_list.push(mon_data);
+            res_list.push(mon.pokemon.name);
         }
         return res_list;
     }
@@ -103,16 +240,11 @@ async function find_pokemon_with_type(name, type) {
         let found = null;
         for(let mon in pkmn_list) {
             if(mon.pokemon.name === name) {
-                found = mon.pokemon;
+                found = name;
             }
         }
         if(found) {
-            const m_res = await fetch(found.url);
-            if(!m_res.ok) {
-                return null;
-            }
-            const m_data = await m_res.json();
-            return m_data;
+            return name;
         }
         else {
             return null;
